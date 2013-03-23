c0f175887451f37fad6b727cf24e18663944ed7f
omer <ibntariq@live.com>
2013-03-23 22:30:16 +0500

    ...

---
+++ Main/EngVer/js/eng.js
@@ -0,0 +1,166 @@
+﻿(function () {
+    'use strict';
+
+    WinJS.Application.onmainwindowactivated = function (e) {
+        if (e.detail.kind == Windows.ApplicationModel.Activation.ActivationKind.launch) {
+            // TODO: startup code here
+        }
+    }
+
+    WinJS.Application.start();
+
+    $(document).ready(function () {
+
+        //Effects at the start//  
+        var x = false;
+        $('#name').transit({ opacity: 1 }, 4000);
+        $('#container').transit({ y: '-250px' }, 1000).delay(3000);
+
+        $('.main_button').transit({ x: '-40%', easing: 'snap' }, 1000);
+        setTimeout(function () {
+            $('.bg_pattern').transit({ opacity: 1 }, 2000);
+        }, 1000)
+
+        //Events on clicks//
+        $('.box').click(function () {
+            if ($('.bg_pattern').css('opacity') == 1) {
+                $('.bg_pattern').transit({ opacity: 0 }, 'slow').delay(300);
+                $('.main_button').transit({ opacity: 0 }, 'slow').css({ display: 'none' });
+                $('.head').transit({ opacity: 0 }, 'slow').css({ display: 'none' });
+                setTimeout(function () {
+                    $('#container').transit({ opacity: 0 }, 'slow');         
+                     }, 400)
+                setTimeout(function () {
+                    $('#container').css({ display: 'none' });
+                }, 600)                
+                setTimeout(function () {
+                    $('#display').fadeIn('slow').delay(3000);
+                }, 1500)
+                setTimeout(function () {
+                    $('#back').fadeIn('fast').transit({ x: "-34%", easing: 'snap' }, 2000);
+                }, 1500)
+            }
+
+        })
+
+        $('#aqd_box').click(function () {
+            if ($('.bg_pattern').css('opacity') == 1) {
+                setTimeout(function () {
+                    $('#aqd_pg').transit({ x: "108%", easing: 'snap' }, 2000);
+                }, 1500)
+            }
+        });
+
+        $('#mnhj_box').click(function () {
+            if ($('.bg_pattern').css('opacity') == 1) {
+                setTimeout(function () {
+                    $('#mnhj_pg').transit({ x: "108%", easing: 'snap' }, 2000);
+                }, 1500)
+            }
+
+        });
+
+        $('#wrshp_box').click(function () {
+            if ($('.bg_pattern').css('opacity') == 1) {
+                setTimeout(function () {
+                    $('#wrshp_pg').transit({ x: "108%", easing: 'snap' }, 2000);
+                }, 1500)
+            }
+        });
+
+        $('#misc_box').click(function () {
+            if ($('.bg_pattern').css('opacity') == 1) {
+                setTimeout(function () {
+                    $('#misc_pg').transit({ x: "108%", easing: 'snap' }, 2000);
+                }, 1500)
+            }
+        });
+
+        $('#abt_box').click(function () {
+            if ($('.bg_pattern').css('opacity') == 1) {
+                setTimeout(function () {
+                    $('#abt_pg').transit({ x: "108%", easing: 'snap' }, 2000);
+                }, 1500)
+            }
+        });
+
+        //Hovering Effects//
+        $('#aqd_box').hover(function () {
+
+            $(this).css({ backgroundColor: "#0D4F8B" }, "slow").mouseleave(function () {
+                $(this).css({ backgroundColor: "#191919" }, "slow");
+            });
+        });
+
+        $('#mnhj_box').hover(function () {
+
+            $(this).css({ backgroundColor: "#5C246E" }, "slow").mouseleave(function () {
+                $(this).css({ backgroundColor: "#191919" }, "slow");
+
+            });
+        });
+
+        $('#wrshp_box').hover(function () {
+
+            $(this).css({ backgroundColor: "#FF3030" }, "slow").mouseleave(function () {
+                $(this).css({ backgroundColor: "#191919" }, "slow");
+
+            });
+        });
+
+        $('#misc_box').hover(function () {
+
+            $(this).css({ backgroundColor: "#327556" }, "slow").mouseleave(function () {
+                $(this).css({ backgroundColor: "#191919" }, "slow");
+
+            });
+        });
+
+        $('#abt_box').hover(function () {
+
+            $(this).css({ backgroundColor: "#507786" }, "slow").mouseleave(function () {
+                $(this).css({ backgroundColor: "#191919" }, "slow");
+
+            });
+        });
+
+        //hovering on buttons
+        $('#close').hover(function () {
+            $(this).attr("src", "images/buttons/hovclose.png").mouseleave(function () {
+                $(this).attr("src", "images/buttons/close.png")
+            })
+        })
+
+        $('#home').hover(function () {
+            $(this).attr("src", "images/buttons/hov_home.png").mouseleave(function () {
+                $(this).attr("src", "images/buttons/home.png")
+            })
+        })
+
+        $('#back').hover(function () {
+            $(this).attr("src", "images/buttons/hovback.png").mouseleave(function () {
+                $(this).attr("src", "images/buttons/back.png")
+            })
+
+        })
+
+        //clicks on buttons
+        $('#back').click(function () {
+            location.reload(true);
+
+        })
+
+        $('#home').click(function () {
+            if ($('.bg_pattern').css('opacity') == 1) {
+                $('#home_button').attr("href", "/default.html");
+            }
+        })
+
+        $('#close').click(function () {
+            window.close();
+        })
+
+
+    });
+
+})();
---
+++ Main/js/main.js
@@ -0,0 +1,110 @@
+﻿(function () {
+    'use strict';
+
+    WinJS.Application.onmainwindowactivated = function (e) {
+        if (e.detail.kind == Windows.ApplicationModel.Activation.ActivationKind.launch) {
+            // TODO: startup code here
+        }
+    }
+
+    WinJS.Application.start();
+
+    $(document).ready(function () {
+               
+        
+        $('#hadith_eng').transit({ opacity: 1 }, 3000);
+        $('#container').transit({ y: '-20%' }, 1000);
+        setTimeout(function () {
+            $('.lang').transit({ opacity: 1 }, 2000);
+        }, 1000)
+
+        //Hovering Effects//
+        $('#eng_box').hover(function () {
+            $(this).css({ backgroundColor: "#0D4F8B" }, "slow").mouseleave(function () {
+                $(this).css({ backgroundColor: "#191919" }, "slow");
+            });                       
+        });
+
+        $('#urdu_box').hover(function () {
+                        $(this).css({ backgroundColor: "#5C246E" }, "slow").mouseleave(function () {
+                $(this).css({ backgroundColor: "#191919" }, "slow");
+            });
+        });
+
+        $('#arb_box').hover(function () {
+            $(this).css({ backgroundColor: "#B0171F" }, "slow").mouseleave(function () {
+                $(this).css({ backgroundColor: "#191919" }, "slow");                
+            });
+        });
+
+
+        //Changing of Hadith     
+        
+
+        //using hoverIntent
+        var arb_config = {
+            over: mouse_over,
+            sensitivity: 3,
+            interval: 300,                 
+            out: mouse_leave    
+        };
+
+        $('#arb_box').hoverIntent(arb_config);
+
+        function mouse_over() {
+            $('#hadith_eng,#hadith_urdu').transit({ opacity: '0' }, 200);
+            if ($('#hadith_arb').css('opacity') == 0) {
+                $('#hadith_arb').stop(true).transit({ opacity: 1 }, 3000);
+            }
+        }
+
+        function mouse_leave() {
+           
+        }
+
+
+        var eng_config = {
+            over: engmouse_over,
+            sensitivity: 3,
+            interval: 300,            
+            out: engmouse_leave
+        };
+
+        $('#eng_box').hoverIntent(eng_config);
+
+        function engmouse_over() {
+            $('#hadith_arb,#hadith_urdu').transit({ opacity: '0' }, 200);
+                if ($('#hadith_eng').css('opacity') == 0) {
+                    $('#hadith_eng').stop(true).transit({ opacity: 1 }, 3000);
+                }
+        }
+
+        function engmouse_leave() {
+
+        }
+
+        var urdu_config = {
+            over: urdumouse_over,
+            sensitivity: 3,
+            interval: 300,            
+            out: urdumouse_leave
+        };
+
+        $('#urdu_box').hoverIntent(urdu_config);
+
+        function urdumouse_over() {
+                $('#hadith_arb,#hadith_eng').transit({ opacity: '0' }, 200);
+                if ($('#hadith_urdu').css('opacity') == 0) {
+                    $('#hadith_urdu').stop(true).transit({ opacity: 1 }, 3000);
+                }
+        }
+
+        function urdumouse_leave() {
+        }       
+
+
+
+        
+    });
+
+})();
